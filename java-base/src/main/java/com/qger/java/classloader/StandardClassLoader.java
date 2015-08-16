0a0146ed1dd6461d92bee259e9b538c0f61dc16a
Imayjiang@gmail.com <Imayjiang@gmail.com>
2015-08-16 20:17:14 +0800

    define the base framework

---
+++ java-base/src/main/java/com/qger/java/classloader/StandardClassLoader.java
@@ -0,0 +1,13 @@
+package com.qger.java.classloader;
+
+import java.security.SecureClassLoader;
+
+/**
+ * </p>
+ * author: jiangbo
+ * time: 2015/08/16 下午1:34.
+ */
+public class StandardClassLoader extends SecureClassLoader {
+
+
+}
---
+++ java-base/src/main/java/com/qger/java/classloader/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *
+ * </p>
+ * author: jiangbo 
+ * time: 2015/08/16 下午2:51.
+ */
+package com.qger.java.classloader;
